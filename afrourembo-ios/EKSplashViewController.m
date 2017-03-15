513977cd569858477fb06fb0aad0b9339eafc45c
Elie Khoury <elie.khoury@keeward.com>
2017-03-15 10:49:35 +0200

    Create SplashVC. Configure AppDelegate

---
+++ afrourembo-ios/EKSplashViewController.m
@@ -0,0 +1,42 @@
+//
+//  EKSplashViewController.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 3/15/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKSplashViewController.h"
+
+static NSString * const kWelcomeSegue = @"splashToWelcomeSegue";
+
+@implementation EKSplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration: 2.0
+                          delay: 2.0
+                        options: UIViewAnimationOptionCurveEaseOut
+                     animations: ^{
+                         
+                     } completion:^(BOOL finished) {
+                         
+                         [self performSegueWithIdentifier:kWelcomeSegue sender:nil];
+                     }];
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ([segue.identifier isEqualToString:kWelcomeSegue]) {
+        
+    }
+}
+
+@end
