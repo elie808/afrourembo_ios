91c96eacfe8eecd5ecfe59e69c54866b56a0e8f0
Elie Khoury <elie.khoury@keeward.com>
2017-03-18 14:14:47 +0200

    Create SignInVC UI

---
+++ afrourembo-ios/EKSignInViewController.h
@@ -0,0 +1,15 @@
+//
+//  EKSignInViewController.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 3/16/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EKSignInViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ afrourembo-ios/EKSignInViewController.m
@@ -0,0 +1,63 @@
+//
+//  EKSignInViewController.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 3/16/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKSignInViewController.h"
+
+static NSString * const kSigninCell = @"signinCell";
+
+@interface EKSignInViewController() {
+    NSArray *_dataSourceArray;
+}
+@end
+
+@implementation EKSignInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Sign in";
+    _dataSourceArray = @[@"Email", @"Password"];
+}
+
+#pragma mark - TableView DataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kSigninCell forIndexPath:indexPath];
+
+    cell.textLabel.text = _dataSourceArray[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
