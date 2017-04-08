5344c4e5118c5468e5e014c161523dc0193cc277
Elie Khoury <elie808@hotmail.com>
2017-04-09 02:11:23 +0300

    Create EKImageTextButton class. Fix roleVC UI layout

---
+++ afrourembo-ios/EKImageTextButton.h
@@ -0,0 +1,13 @@
+//
+//  EKImageTextButton.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/9/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EKImageTextButton : UIButton
+
+@end
---
+++ afrourembo-ios/EKImageTextButton.m
@@ -0,0 +1,21 @@
+//
+//  EKImageTextButton.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/9/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKImageTextButton.h"
+
+@implementation EKImageTextButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
