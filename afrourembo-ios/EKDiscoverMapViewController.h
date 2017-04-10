9cdd11a2d7315a095e67ceb7b529d2f7a421c090
Elie Khoury <elie808@hotmail.com>
2017-04-11 02:07:28 +0300

    Add map to DiscoverMapVC and link MapKit framework to project

---
+++ afrourembo-ios/EKDiscoverMapViewController.h
@@ -0,0 +1,18 @@
+//
+//  EKDiscoverMapViewController.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/11/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <MapKit/MapKit.h>
+
+@interface EKDiscoverMapViewController : UIViewController <UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate>
+
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ afrourembo-ios/EKDiscoverMapViewController.m
@@ -0,0 +1,29 @@
+//
+//  EKDiscoverMapViewController.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/11/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKDiscoverMapViewController.h"
+
+@implementation EKDiscoverMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+ 
+    self.navigationItem.titleView = self.searchBar;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
