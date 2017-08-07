784c0b1dee3fe53ab51f16549f68661e6d330180
Elie Khoury <elie808@hotmail.com>
2017-08-07 09:29:00 +0300

    Remove unused properties from Review class

--- afrourembo-ios/Review.h
+++ afrourembo-ios/Review.h
@@ -10,16 +10,6 @@
 
 @interface Review : NSObject
 
-//@property NSString *reviewTitle;
-//@property NSNumber *reviewStars;
-//@property NSString *reviewAuthor;
-//@property NSString *reviewText;
-//@property NSString *reviewDate; //TODO: Change to NSDate
-//
-////TODO: Replace with professional NSObject
-//@property NSString *reviewProfessional; // reviewed professional
-//@property NSString *reviewProfessionalImage; // reviewed professional profile image
-
 @property NSString *reviewerFirstName;
 @property NSString *reviewerLastName;
 @property NSString *reviewerProfilePicture;
