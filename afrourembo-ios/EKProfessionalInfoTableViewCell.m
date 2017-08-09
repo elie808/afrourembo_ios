eaef0d2f385ad4c1babd8c3006d0ab15bea13893
Elie Khoury <elie808@hotmail.com>
2017-08-09 11:55:13 +0300

    Create professionalInfoCell class

---
+++ afrourembo-ios/EKProfessionalInfoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  EKProfessionalInfoTableViewCell.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 8/9/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKProfessionalInfoTableViewCell.h"
+
+@implementation EKProfessionalInfoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
