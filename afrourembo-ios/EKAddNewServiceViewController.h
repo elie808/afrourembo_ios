37dfb7d12318f54210d3ee40171021946d853227
Elie Khoury <elie808@hotmail.com>
2017-08-09 14:49:55 +0300

    Add done button to number keyboard, and DatePicker for price field in addServiceVC

--- afrourembo-ios/EKAddNewServiceViewController.h
+++ afrourembo-ios/EKAddNewServiceViewController.h
@@ -11,14 +11,19 @@
 #import "EKAddNewServiceTableViewCell.h"
 #import "Service.h"
 #import "Category.h"
+#import "NSDate+Helpers.h"
 
 @interface EKAddNewServiceViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
 @property (strong, nonatomic) Service *passedService;
 @property (strong, nonatomic) Service *serviceToEdit; // to be passed down to presented segues, and updated by unwinding vc's
 
+@property (strong, nonatomic) IBOutlet UIView *timePickerView;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) IBOutlet UIButton *removeServiceButton;
+@property (strong, nonatomic) UIToolbar *keyboardToolbar;
+
+- (IBAction)didChangeTimePicker:(UIDatePicker *)sender;
 
 - (IBAction)unwingToAddNewServiceVC:(UIStoryboardSegue *)segue;
 
