4f31b61e68aedfc4574b0a0940e856108d810573
Elie Khoury <elie808@hotmail.com>
2017-08-21 01:38:46 +0300

    Enable editing and uploading of customer profile pictures

--- afrourembo-ios/EKEditProfileInfoViewController.m
+++ afrourembo-ios/EKEditProfileInfoViewController.m
@@ -8,6 +8,8 @@
 
 #import "EKEditProfileInfoViewController.h"
 
+static int const kMaxImageSize = 0.3; //MBs
+
 static NSString * const keditProfileInfoCell = @"editProfileInfoCell";
 static NSString * const kExploreSegue = @"editVcToExploreVC";
 
@@ -69,7 +71,36 @@ static NSString * const kExploreSegue = @"editVcToExploreVC";
     
 }
 
+#pragma mark - UIImagePickerControllerDelegate
+
+// This method is called when an image has been chosen from the library or taken from the camera.
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [ProfilePicture uploadCustomerProfilePicture:[UIImage compressImage:image toSize:kMaxImageSize]
+                                       withToken:self.passedUser.token
+                                       withBlock:^(Customer *customer) {
+                                           
+                                           [MBProgressHUD hideHUDForView:self.view animated:YES];
+                                           self.profilePicImageView.image = image;
+                                       }
+                                      withErrors:^(NSError *error, NSString *errorMessage, NSInteger statusCode) {
+                                          
+                                          [MBProgressHUD hideHUDForView:self.view animated:YES];
+                                          [self showMessage:errorMessage withTitle:@"Error" completionBlock:nil];
+                                      }];
+}
+
 #pragma mark - Actions
+
+- (IBAction)didTapChageProfilePicture:(id)sender {
+    
+    [self presentAlertController];
+}
 
 - (IBAction)didTapSubmitButton:(id)sender {
 
@@ -115,6 +146,54 @@ static NSString * const kExploreSegue = @"editVcToExploreVC";
     if ([segue.identifier isEqualToString:kExploreSegue]) {
         
     }
+}
+
+#pragma mark - Helpers
+
+- (void)presentAlertController {
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Upload your profile picture"
+                                                                             message:nil
+                                                                      preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *myLocationsAction = [UIAlertAction actionWithTitle:@"from Photo Gallery"
+                                                                style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction *action) {
+                                                                  [self openPhotoGallery];
+                                                              }];
+    
+    UIAlertAction *friendsLocationsAction = [UIAlertAction actionWithTitle:@"from Camera"
+                                                                     style:UIAlertActionStyleDefault
+                                                                   handler:^(UIAlertAction *action) {
+                                                                       [self openCamera];
+                                                                   }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:^(UIAlertAction *action) {}];
+    
+    [alertController addAction:myLocationsAction];
+    [alertController addAction:friendsLocationsAction];
+    [alertController addAction:cancelAction];
+    
+    [self presentViewController:alertController animated:YES completion:^{}];
+}
+
+- (void)openPhotoGallery {
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+}
+
+- (void)openCamera {
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    imagePickerController.cameraDevice=UIImagePickerControllerCameraDeviceFront;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
 }
 
 @end
