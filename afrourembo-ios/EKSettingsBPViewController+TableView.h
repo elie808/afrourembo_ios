486b8c4cf29ae829668abb7914ecdb95822b9cf7
Elie Khoury <elie808@hotmail.com>
2017-10-12 03:17:41 +0300

    Add forward arrow assets to project

---
+++ afrourembo-ios/EKSettingsBPViewController+TableView.h
@@ -0,0 +1,13 @@
+//
+//  EKSettingsBPViewController+TableView.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 10/12/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKSettingsBPViewController.h"
+
+@interface EKSettingsBPViewController (TableView)
+
+@end
---
+++ afrourembo-ios/EKSettingsBPViewController+TableView.m
@@ -0,0 +1,13 @@
+//
+//  EKSettingsBPViewController+TableView.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 10/12/17.
+//  Copyright © 2017 Elie El Khoury. All rights reserved.
+//
+
+#import "EKSettingsBPViewController+TableView.h"
+
+@implementation EKSettingsBPViewController (TableView)
+
+@end
