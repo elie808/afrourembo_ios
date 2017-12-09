e4d9d1a00fcf76d96fcf1932542ed271beef027e
Elie Khoury <elie808@hotmail.com>
2017-12-09 14:08:19 +0200

    Add support to salon persistence methods to save most Salon properties

--- afrourembo-ios/Salon+Helpers.m
+++ afrourembo-ios/Salon+Helpers.m
@@ -33,6 +33,13 @@
     delta.salonID = newSalon.salonID.length > 0 ? newSalon.salonID : existingSalon.salonID;
     delta.token = newSalon.token.length > 0 ? newSalon.token : existingSalon.token;
     
+    delta.fName = newSalon.fName.length > 0 ? newSalon.fName : existingSalon.fName;
+    delta.lName = newSalon.lName.length > 0 ? newSalon.lName : existingSalon.lName;
+    delta.email = newSalon.email.length > 0 ? newSalon.email : existingSalon.email;
+    delta.name  = newSalon.name.length > 0 ? newSalon.name : existingSalon.name;
+    delta.address = newSalon.address.length > 0 ? newSalon.address : existingSalon.address;
+    delta.phone = newSalon.phone.length > 0 ? newSalon.phone : existingSalon.phone;
+
     return delta;
 }
 
@@ -40,7 +47,13 @@
     
     NSDictionary *details = @{
                               @"salonID" : self.salonID.length > 0 ? self.salonID : @"",
-                              @"token" : self.token.length > 0 ? self.token : @""
+                              @"token" : self.token.length > 0 ? self.token : @"",
+                              @"fName" : self.fName.length > 0 ? self.fName : @"",
+                              @"lName" : self.lName.length > 0 ? self.lName : @"",
+                              @"email" : self.email.length > 0 ? self.email : @"",
+                              @"name" : self.name.length > 0 ? self.name : @"",
+                              @"address" : self.address.length > 0 ? self.address : @"",
+                              @"phone" : self.phone.length > 0 ? self.phone : @""
                               };
     
     NSError *error;
