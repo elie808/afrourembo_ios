796c178675e0f46f4446e14b24933ac1928c33f1
Elie Khoury <elie808@hotmail.com>
2018-01-22 17:34:49 +0200

    Create vendorBooking class to map returned vendor bookings/slots

---
+++ afrourembo-ios/VendorBookings.h
@@ -0,0 +1,18 @@
+//
+//  VendorBookings.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 1/22/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VendorBookings : NSObject
+
+@property NSString *userId;
+@property NSString *userType;
+@property NSDate *fromTime;
+@property NSDate *toTime;
+
+@end
---
+++ afrourembo-ios/VendorBookings.m
@@ -0,0 +1,13 @@
+//
+//  VendorBookings.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 1/22/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+#import "VendorBookings.h"
+
+@implementation VendorBookings
+
+@end
