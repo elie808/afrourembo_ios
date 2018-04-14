fda0ceee3446de45dd0cb3be79e3faac52d5a0d8
Elie Khoury <elie808@hotmail.com>
2018-04-14 19:14:06 +0300

    Fix wrong unwind segue from addServiceVC to BpSettingsVC

---
+++ afrourembo-ios/EKPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  EKPaymentViewController.h
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/14/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EKPaymentViewController : UIViewController
+
+@end
---
+++ afrourembo-ios/EKPaymentViewController.m
@@ -0,0 +1,37 @@
+//
+//  EKPaymentViewController.m
+//  afrourembo-ios
+//
+//  Created by Elie El Khoury on 4/14/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+#import "EKPaymentViewController.h"
+
+@interface EKPaymentViewController ()
+
+@end
+
+@implementation EKPaymentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
